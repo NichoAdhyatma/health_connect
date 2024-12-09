58ba8950fe7aecac6ef4f52d31a385791a376c66
NichoAdhyatma <nicholausadhyatma@gmail.com>
2024-12-09 20:56:17 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/health/connect/health_connect/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.health.connect.health_connect
+
+import io.flutter.embedding.android.FlutterActivity
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:health_connect/page/home_page.dart';
+
+void main() {
+  runApp(const HealthApp());
+}
+
+
